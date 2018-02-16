fe6967a22ca85a26035b7a0b28a68b632d00dfa1
Kristian Salo <salok2@aalto.fi>
2018-02-16 14:28:02 +0200

    Implemented displaying the fetched data.

--- src/SightingTable.js
+++ src/SightingTable.js
@@ -1,35 +1,44 @@
-import React, { Component } from 'react';
-import { Table } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Table } from "react-bootstrap";
 
 
 
 class SightingTable extends Component {
-	constructor(props) {
+  constructor(props) {
     super(props);
   }
   
-  render() {//Change to arrows so that only the other is shown
+  render() {
+    const tableData = this.props.data.map(sighting => {
+      const date = new Date(sighting.dateTime);
+      const formattedDate = date.getDay() + "." + date.getMonth() + "." + date.getFullYear() +
+                            " " + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2);
+      return (
+        <tr>
+          <td> {sighting.species} </td>
+          <td> {sighting.count} </td>
+          <td> {formattedDate} </td>
+          <td> {sighting.description} </td>
+        </tr>
+      );
+    });
+    //if the table is empty
     return(
-    <div>
-      <Table striped bordered condensed hover responsive>
-        <thead> 
-          <tr>
-            <th className="col-md-2"> Species </th> 
-            <th className="col-md-1"> Count </th> 
-            <th className="col-md-3"> Time <span style={{textAlign:'right', float:'right'}}> &#9206; &#9207; </span> </th> 
-            <th className="col-md-5"> Description </th> 
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td> Mallard </td>
-            <td> 2 </td>
-            <td> 14.12.2018 13:33</td>
-            <td> A horrendeous one. </td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
+      <div>
+        <Table striped bordered condensed hover responsive>
+          <thead> 
+            <tr>
+              <th className="col-md-2"> Species </th> 
+              <th className="col-md-1"> Count </th> 
+              <th className="col-md-3"> Time <span style={{textAlign:'right', float:'right'}}> &#9206; &#9207; </span> </th> 
+              <th className="col-md-5"> Description </th> 
+            </tr>
+          </thead>
+          <tbody>
+            {tableData}
+          </tbody>
+        </Table>
+      </div>
     );
   }
 }
